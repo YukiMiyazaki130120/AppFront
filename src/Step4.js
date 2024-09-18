481db0785ebcc8a62980f9219ba4501cd34ddfae
SaiZyoken <xalice999@outlook.jp>
2024-09-18 16:18:33 +0900

    Add ErrorProcess

--- src/Step4.js
+++ src/Step4.js
@@ -5,11 +5,26 @@ import logo2 from './Image/wallet.png';
 import header_logo from './Image/icon.png';
 import menu from './Image/menu.png';
 import './Step4.css';
+import React, { useState } from 'react';
 
 function Top() {
   const navigate = useNavigate()
+
+  const [money, setState] = useState(0);
+  const [errors, setErrors] = useState({});
+
   const handleRemit = () => {
-    navigate('/Step5_complete')
+    if(money > 50000){
+      const newErrors = {};
+      newErrors.money = '送金上限額を超えています';
+      setErrors(newErrors);
+    }else if(money <= 0){
+      const newErrors = {};
+      newErrors.money = '不正な金額です';
+      setErrors(newErrors);
+    }else{
+      navigate('/Step5_complete')
+    }
   }
   return (
     <div>
@@ -28,7 +43,14 @@ function Top() {
         <div class="box2">
           <p>50000円</p>
         </div>
-        <input type="number" placeholder="金額を入力" class="input-box" />
+        {errors.money && <span className="error">{errors.money}</span>}
+        <input type="number"
+        placeholder="金額を入力"
+        class="input-box"
+        min={0}
+        value={money}
+        onChange={(e) => setState(e.target.value)}
+        />  
         <div class="p-test01">
           <a class="btn" onClick={handleRemit}>
             <div class="flex_2">送金</div>
