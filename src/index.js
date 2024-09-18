000915a009e673279f63978efb1a4b51134120c7
YukiMiyazaki13120 <hyuki13120@gmail.com>
2024-09-18 10:13:27 +0900

    modified step5

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
-      <Top />
+      < Step5_complete/>
     </React.StrictMode>
 );
 
