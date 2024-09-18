2608e0ba00ff969e86cf8c99616ee6be0d8dd60e
s1f102202493 <107388720+s1f102202493@users.noreply.github.com>
2024-09-18 15:31:00 +0900

    Top画面で、データベースからとってきた値を表示

--- src/Top.js
+++ src/Top.js
@@ -5,43 +5,76 @@ import logo2 from './Image/wallet.png';
 import header_logo from './Image/icon.png';
 import menu from './Image/menu.png';
 import './Top.css';
-//import axios from 'axios';
+import axios from 'axios';
+import React from "react";
 
 
 
 function Top() {
+  const [post, setPost] = React.useState([]);
+  const [filename, setFilename] = React.useState("");
+
+  
+  React.useEffect(() => {
+    axios.get('http://localhost:5000/top').then((response) => {
+    setPost(response.data);
+    console.log(response.data);
+    setFilename("/Image/" + response.data.image_path);
+    })
+  }, []);
+  
+
   const navigate = useNavigate()
   const handleRemit = () => {
     navigate('/RemittanceDest')
   }
   /*
-  axios.get('http://localhost:5000/')
+  let data;
+  axios.get('http://localhost:5000/top')
     .then(responce => {
-      const message = responce.data.message;
-      console.log(message);
+      data = responce.data;
+      console.log(data);
     })
     .catch(error => {
       console.error('リクエストエラー:', error);
     });
   */
 
+    // let data;
+    // async function fetchData() {
+    //   try {
+    //     const response = await fetch('http://localhost:5000/top');
+    //     data = await response.data;
+    //     // データを処理するコードをここに記述
+    //     console.log(data);
+    //   } catch (error) {
+    //     console.error('リクエストエラー:', error);
+    //   }
+    // }
+    // fetchData();
+    // console.log("a");
 
+
+  //const logo = "/Image/human1.png";
+
+    
   return (
     <div>
       <div class="header">
         <figure class="header_logo"><img src={header_logo} alt="ロゴ画像"/></figure>
         <figure class="menu"><img src={menu} alt="ロゴ画像"/></figure>
       </div>
-
+      
       <div class="flex">
-        <figure class="image"><img src={logo} alt="ロゴ画像"/></figure>
+        <figure class="image"><img src={filename} alt="ロゴ画像"/></figure>
         <p class="text">サンプル　氏名</p>
       </div>
-      <h2 class="text">口座番号</h2>
+      
+      <h2 class="text">口座番号:  {post.account_num}</h2>
       <h3 class="text">預金残高</h3>
       <div class="container">
         <div class="box2">
-          <p>50000円</p>
+          <p>{post.balance}</p>
         </div>
         <Button onClick={handleRemit}>送金</Button>
       </div>
